e5f3c17c324286a7382e45e755209abb9fea88f2
Gavin S <stahlgavin@gmail.com>
2022-06-06 17:55:53 -0700

    basic api calls setup and working

--- src/components/Factoftheday.js
+++ src/components/Factoftheday.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Factoftheday() {
+  const [fact, setFact] = useState();
+  useEffect(() => {
+    axios
+      .get("https://uselessfacts.jsph.pl/today.json?language=en")
+      .then((data) => {
+        setFact(data.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <div>
-      <p>fact of the day!</p>
+      <p>{fact ? fact.text : "Loading..."}</p>
     </div>
-  )
+  );
 }
-
--- src/components/Randomfact.js
+++ src/components/Randomfact.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Randomfact() {
+  const [fact, setFact] = useState();
+  useEffect(() => {
+    axios
+      .get("https://uselessfacts.jsph.pl/random.json?language=en")
+      .then((data) => {
+        setFact(data.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <div>
-      <p>random fact!</p>
+      <p>{fact ? fact.text : "Loading..."}</p>
     </div>
-  )
+  );
 }
